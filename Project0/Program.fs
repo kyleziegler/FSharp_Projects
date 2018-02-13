2922bea7818ca4251848fef334a7b7efb810e7a8
Kyle Ziegler <kziegler17@gmail.com>
2018-02-12 17:54:49 -0800

    Init commit

---
+++ Project0/Program.fs
@@ -0,0 +1,29 @@
+// Kyle Ziegler 2018
+// Functional programming practice.
+
+namespace Project0
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+
+module Program =
+    let exitCode = 0
+
+    let BuildWebHost args =
+        WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build()
+
+    [<EntryPoint>]
+    let main args =
+        BuildWebHost(args).Run()
+
+        exitCode
