4070fabcd4173bc4d3beb9c42d151b38701d9f33
Kyle Ziegler <kziegler17@gmail.com>
2018-02-13 16:02:57 -0800

    More list functions added

--- Project0/Program.fs
+++ Project0/Program.fs
@@ -3,16 +3,32 @@
 
 open System
 
-
 //Find the last element in a given list.
 module Lists =
     let rec lastListElement list=
         match list with
         | [] -> failwith "List empty"
-        | [item] -> item //When we only have one item in the list, return it.
+        | [last] -> last //When we only have one item in the list, return it.
         | _::remainingList -> lastListElement remainingList //call the function with the remaining list 
 
     //Print the integer result
-    let run = lastListElement [1;2;3;]
-    printfn "Result = %i" run
+    let test1 = lastListElement [1 .. 100]
+    printfn "Result = %i" test1
 
+    let rec secondToLast list = 
+        match list with
+        | [] -> failwith "Empty list"
+        | [a] -> failwith "Only one element in the list"
+        | [e;_] -> e //found the right one
+        | _::remainingList -> secondToLast remainingList
+
+    let test2 = secondToLast [1 .. 100]
+    printfn "Result = %i" test2
+
+    let rec numElements list sum = 
+        match list with 
+        | [] -> sum
+        | _::restOfList -> numElements restOfList (sum+1)
+
+    let test3 = numElements [1 .. 100] 0
+    printfn "Result = %i" test3
