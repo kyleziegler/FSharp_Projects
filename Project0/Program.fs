b46ecb1ceaa41d04ff1e5afdaed85ec55f39b294
Kyle Ziegler <kziegler17@gmail.com>
2018-05-15 10:52:46 -0700

    Added list concat

--- Project0/Program.fs
+++ Project0/Program.fs
@@ -1,20 +1,23 @@
 //Kyle Ziegler 2018
 //F# functional programming practice.
+//Problems solved from https://wiki.haskell.org/99_questions/1_to_10
 
 open System
 
-//Find the last element in a given list.
+//All list problems
 module Lists =
+    //Return the last element in a list
     let rec lastListElement list=
         match list with
         | [] -> failwith "List empty"
         | [last] -> last //When we only have one item in the list, return it.
         | _::remainingList -> lastListElement remainingList //call the function with the remaining list 
 
-    //Print the integer result
     let test1 = lastListElement [1 .. 100]
     printfn "Result = %i" test1
 
+    //Return the second to last element in a list
+    //Fail on empty sets and single element sets
     let rec secondToLast list = 
         match list with
         | [] -> failwith "Empty list"
@@ -25,6 +28,7 @@ module Lists =
     let test2 = secondToLast [1 .. 100]
     printfn "Result = %i" test2
 
+    //Count the elements in a list
     let rec numElements list sum = 
         match list with 
         | [] -> sum
@@ -32,3 +36,17 @@ module Lists =
 
     let test3 = numElements [1 .. 100] 0
     printfn "Result = %i" test3
+
+    let flattenList l = List.concat l
+
+    //Replace a list of lists with a singular flat list.
+    //let rec flattenList l acc= 
+        //match (l,acc) with
+        //| ([],[]) -> failwith "List empty"
+        //| ([],acc) -> acc
+        //| (x::xs, acc) -> flattenList xs List.append(x,acc)
+
+    
+    let test4 = flattenList [[1;2;3];[4;5;6];[7;8;9]]
+    test4 |> List.iter (fun x -> printf "%d " x)
+
